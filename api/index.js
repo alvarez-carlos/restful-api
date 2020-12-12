89f61cb9ef3f258fbefa9c033d44a66564bc5a15
carlos-alvarez <alvarezcarlosa507@gmail.com>
2020-12-12 15:43:26 -0500

    Added DB Conn

--- api/index.js
+++ api/index.js
@@ -4,27 +4,27 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-//Declare routues
+//Declare routes
 const Subjects = require('./routes/subjects');
 const Enrolments = require('./routes/enrolments');
 const Auth = require('./routes/authentication');
 
 //Create express app
-const app = express()
+const app = express();
 
 //Adding dependencies to the express app
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cors());
 
 //Connection string to our MongoDB
-
+mongoose.connect(process.env.EnvVar_MongoConnectionString, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //Calling our routes
-app.use('/api/subjects', Subjects)
-app.use('/api/enrolments', Enrolments)
-app.use('/api/auth', Auth)
+app.use('/api/subjects', Subjects);
+app.use('/api/enrolments', Enrolments);
+app.use('/api/auth', Auth);
 
-module.exports = app
+module.exports = app;
 
 
 
