e712e06b0c772e84c820082fd7166dd51d6ac8ea
carlos-alvarez <alvarezcarlosa507@gmail.com>
2020-12-14 01:01:05 -0500

    Created Models Subjects, Enrolments, Students

--- api/models/students.js
+++ api/models/students.js
@@ -1,1 +1,14 @@
 //Students Schema
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Students = mongoose.model('Student', new Schema({
+  name;String,
+  email: String,
+  password: String,
+  salt: String,
+  role: { type: String, default: 'user' } //student, professor,..
+}));
+
+module.exports = Students;
--- api/models/subjects.js
+++ api/models/subjects.js
@@ -1,1 +1,11 @@
 //Subjetcs Schema
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Subjects = mongoose.model('Subject', new Schema({
+  codigo: String,
+  desc: String,
+}));
+
+module.exports = Subjects;
